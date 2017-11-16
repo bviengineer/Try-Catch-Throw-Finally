3745401908d95c07e4233b037fb784d923c96873
Kaisma Penn-Titley <kptitley@gmail.com>
2017-11-16 12:46:19 -0500

    index page: removed additional angle bracket & exclamation mark from doctype declration | corrected html entitiy for copyright symbol | js file: added code for try catch throw finally

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 // 1. Try a try/catch 
 // 2. Throw an error
 // 3. Handle the error
+
+    var userInput = Number(prompt("enter any whole number"));
+    if(isNaN(userInput) == true){
+        alert("you did not enter a number");
+    } else {
+        document.querySelector("#displayDiv").innerHTML = 'You entered the number ' + '"' + userInput + '"';
+        displayDiv.style.color = "white";
+        displayDiv.style.backgroundColor = "black";
+        displayDiv.style.fontSize = "5rem" 
+    }
+
